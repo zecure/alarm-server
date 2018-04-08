ca87eed3db84d52e794471db13d03efa61590db5
zit-hb <hb@zecure.org>
2018-04-08 14:41:21 +0200

    Send all notifications to admin users

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -7,59 +7,79 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Alarm;
 use App\Form\AlarmType;
+use App\Entity\User;
 
 class UploadController extends Controller
 {
     /**
      * @param Request $request
-     * @param \Swift_Mailer $mailer
      * @return Response
      */
-    public function upload(Request $request, \Swift_Mailer $mailer) : Response
+    public function upload(Request $request) : Response
     {
         $alarm = new Alarm();
         $form = $this->createForm(AlarmType::class, $alarm);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
-            $file = $alarm->getFile();
-            $fileName = hash('sha256', random_bytes(16)) . '.' . $file->guessExtension();
-
-            $file->move(
-                $this->getParameter('upload_directory'),
-                $fileName
-            );
-
-            $alarm->setFile($fileName);
-            $alarm->setFileName($file->getClientOriginalName());
-            $alarm->setCreatedBy($this->getUser()->getUsername());
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($alarm);
-            $em->flush();
-
-            $message = (new \Swift_Message($this->getParameter('mail_subject_alarm')))
-                ->setFrom($this->getParameter('mailer_from'))
-                ->setTo($this->getUser()->getEmail())
-                ->setBody(
-                    $this->renderView(
-                    'alarm/email.txt.twig',
-                        ['alarm' => $alarm]
-                    ),
-                    'text/plain'
-                )
-                ->attach(\Swift_Attachment::fromPath($this->getParameter('upload_directory') . '/' . $fileName))
-            ;
-
-            $mailer->send($message);
-
-            return new Response();
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('alarm/upload.html.twig', [
+                'form' => $form->createView()
+            ]);
         }
 
-        return $this->render('alarm/upload.html.twig', [
-            'form' => $form->createView()
-        ]);
+        /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
+        $file = $alarm->getFile();
+        $fileName = hash('sha256', random_bytes(16)) . '.' . $file->guessExtension();
+
+        $file->move(
+            $this->getParameter('upload_directory'),
+            $fileName
+        );
+        $newPath = $this->getParameter('upload_directory') . '/' . $fileName;
+
+        $alarm->setFile($fileName);
+        $alarm->setFileName($file->getClientOriginalName());
+        $alarm->setCreatedBy($this->getUser()->getUsername());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($alarm);
+        $em->flush();
+
+        /** @var \App\Repository\UserRepository $userRepository */
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+
+        $adminUsers = $userRepository->findAdmins();
+
+        foreach ($adminUsers as $adminUser) {
+            $this->notify($adminUser, $alarm, $newPath);
+        }
+
+        return new Response();
+    }
+
+    /**
+     * @param User $adminUser
+     * @param Alarm $alarm
+     * @param string $newPath
+     */
+    private function notify(User $adminUser, Alarm $alarm, string $newPath)
+    {
+        /** @var \Swift_Mailer $mailer */
+        $mailer = $this->get('mailer');
+        $message = (new \Swift_Message($this->getParameter('mail_subject_alarm')))
+            ->setFrom($this->getParameter('mailer_from'))
+            ->setTo($adminUser)
+            ->setBody(
+                $this->renderView(
+                    'alarm/email.txt.twig',
+                    ['alarm' => $alarm]
+                ),
+                'text/plain'
+            )
+            ->attach(\Swift_Attachment::fromPath($newPath))
+        ;
+
+        $mailer->send($message);
     }
 }
